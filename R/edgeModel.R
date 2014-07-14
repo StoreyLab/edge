d13daa9f1dc2f1715fb6eeaeb173f8a0e3bd5fba
andy <andy@eve.(none)>
2014-07-14 10:40:34 -0400

    significant changes to edgeModel

--- R/edgeModel.R
+++ R/edgeModel.R
@@ -7,6 +7,7 @@
 #' @param grp vector- Groups or biological variable in experiment. Optional.
 #' @param tme vector- Covariate of interest in time course study. Optional. 
 #' @param ind factor- Individual factor. Optional. 
+#' @param bio.var matrix- Biological variables. Optional.
 #' @param basis.df numeric- Degree of freedom of the spline fit for time course study. Default is 2.
 #' @param basis.type string- Either "ncs" or "ps" basis for time course study. Default is "ncs".
 #' @param adj.var matrix- Adjustment Variables. Optional.
@@ -37,69 +38,116 @@
 #' @author John Storey, Andy Bass 
 #' @aliases edgeModel
 #' @export
-edgeModel = function(data, sampling=c("static", "timecourse"), tme=NULL, ind=NULL, basis.df=2, basis.type = c("ncs", "poly"), grp=NULL, adj.var=NULL) {
+edgeModel = function(data, sampling=c("static", "timecourse"), tme=NULL, ind=NULL, basis.df=2, basis.type = c("ncs", "poly"), grp=NULL, adj.var=NULL, bio.var=NULL) {
   n <- ncol(data)
   m <- nrow(data)
-  sampling <- match.arg(sampling, choices=c("static", "timecourse"))
-  if (!is.null(grp)) {
-    grp <- as.matrix(grp)
-  } else {
-    if(sampling == "static") {
-      stop("grp variable cannot be missing for static sampling.")
-    }
-    grp <- as.matrix(rep(1, n), nrow=1)
+  if (!is.matrix(data)) {
+    stop("data must be a matrix")
   }
-  g <- nrow(unique(grp))
-
-  if (sampling == "static") {
-    if (g==1) {
-      stop("grp must have more than one unique value for static sampling.")
+  if (!is.null(tme)) {
+    if (is.matrix(tme) | is.vector(tme)) {
+      tme <- data.frame(tme)
+    } else {
+      stop("tme must be a matrix/vector")
     }
-    
-   # pdat <- data.frame(adj.var, grp)
+    intercept <- !apply(tme, 2, var)
+    tme <- subset(tme, select=!intercept)
+  }
+  
+  if (!is.null(adj.var)) {
+    if (is.matrix(adj.var) | is.data.frame(adj.var) | is.vector(adj.var)) {
+      adj.var <- data.frame(adj.var)
+    } else {
+      stop("adj.var must be a matrix/vector/factor/data.frame")
+    }
+    intercept <- !apply(adj.var, 2, var)
+    adj.var <- subset(adj.var, select=!intercept)
+  }
+  if (!is.null(bio.var)) {
+    sampling <- "notApplicable"
+    if (is.matrix(bio.var) | is.vector(bio.var)) {
+      bio.var <- data.frame(bio.var)
+    } else {
+      stop("bio.var must be a matrix/vector/factor")
+    }
+    intercept <- !apply(bio.var, 2, var)
+    bio.var <- subset(bio.var, select=!intercept)
+    # Create models
     if (is.null(adj.var)) {
-      pdat <- data.frame(grp)
-      nmod <- ~1 
+      pdat <- data.frame(bio.var)
       fmod <- paste("~", paste(names(pdat), collapse=" + "))
-    } else {  
-      pdat <- data.frame(adj.var, grp)
+      nmod <- "~1"
+    } else {
+      pdat <- data.frame(adj.var, bio.var)
       fmod <- paste("~", paste(names(pdat), collapse=" + "))
       nmod <- paste("~", paste(names(adj.var), collapse=" + ")) 
     }
-  }
-  
-  if (sampling == "timecourse") {
-    basis.type <- match.arg(basis.type)
-    varName <- colnames(data.frame(tme))
-    if (length(varName) != 1) stop("Only one time variable is allowed. See ?edgeSet for information on how to create complicated models")
-    if (basis.type == "ncs") {
-      time.basis <- paste("ns(", varName,", df=", basis.df,", intercept=FALSE)", sep="")
-    } else if (basis.type == "poly") {
-      time.basis <- paste("bs(", varName,", df=", basis.df,", intercept=FALSE)", sep="")
-    }
-    if (g == 1) {
-      # time course with no groups
-      pdat <- data.frame(adj.var, tme) 
-      if (is.null(adj.var)) {
-        nmod <- "~1" 
-        fmod <- paste("~", time.basis) 
+  } else {
+    sampling <- match.arg(sampling, choices=c("static", "timecourse")) 
+    if (!is.null(grp)) {
+      if (is.factor(grp)) {
+        grp <- data.frame(model.matrix(~grp))
       } else {
-        fmod <- paste("~", paste(names(adj.var), collapse=" + "), "+", time.basis) 
-        nmod <- paste("~", paste(names(adj.var), collapse=" + ")) 
-      }  
-    } else {
-      if (is.null(adj.var)) {
-        pdat <- data.frame(tme, grp)
-      } else {
-        pdat <- data.frame(tme, adj.var, grp) 
+        stop("grp must be a factor")
       }
-      # time course with groups
-      nmod <- paste(paste("~", paste(names(pdat)[-1], collapse=" + ")), "+", time.basis)
-      fmod <- paste(paste("~", paste(names(pdat)[-1], collapse=" + ")),"+",time.basis,"+", paste( "(", paste(names(pdat)[ncol(pdat)], collapse=" + ", sep=""), ")", ":", time.basis))  }
+      intercept <- !apply(grp, 2, var)
+      grp <- subset(grp, select=!intercept)
+    } else {
+      if(sampling == "static") {
+        stop("grp variable cannot be missing for static sampling.")
+      }
+      grp <- data.frame(grp=rep(1,n))
+    }
+    g <- length(unique(grp))
+    if (sampling == "static") {
+      if (g==1) {
+        stop("grp must have more than one unique value for static sampling.")
+      }
+      if (is.null(adj.var)) {
+        pdat <- data.frame(grp)
+        nmod <- "~1" 
+        fmod <- paste("~", paste(names(pdat), collapse=" + "))
+      } else {  
+        pdat <- data.frame(adj.var, grp)
+        fmod <- paste("~", paste(names(pdat), collapse=" + "))
+        nmod <- paste("~", paste(names(adj.var), collapse=" + ")) 
+      }
+    }
+  
+    if (sampling == "timecourse") {
+      basis.type <- match.arg(basis.type)
+      varName <- colnames(data.frame(tme))
+      if (length(varName) != 1) stop("Only one time variable is allowed. See ?edgeSet for information on how to create complicated models")
+      if (basis.type == "ncs") {
+        time.basis <- paste("ns(", varName,", df=", basis.df,", intercept=FALSE)", sep="")
+      } else if (basis.type == "poly") {
+        time.basis <- paste("bs(", varName,", df=", basis.df,", intercept=FALSE)", sep="")
+      }
+      if (g == 1) {
+        # time course with no groups
+        if (is.null(adj.var)) {
+          pdat <- data.frame(tme) 
+          nmod <- "~1" 
+          fmod <- paste("~", time.basis) 
+        } else {
+          pdat <- data.frame(adj.var, tme) 
+          fmod <- paste("~", paste(names(adj.var), collapse=" + "), "+", time.basis) 
+          nmod <- paste("~", paste(names(adj.var), collapse=" + ")) 
+        }  
+      } else {
+        if (is.null(adj.var)) {
+          pdat <- data.frame(tme, grp)
+        } else {
+          pdat <- data.frame(tme, adj.var, grp) 
+        }
+        # time course with groups
+        nmod <- paste(paste("~", paste(names(pdat)[-1], collapse=" + ")), "+", time.basis)
+        fmod <- paste(paste("~", paste(names(pdat)[-1], collapse=" + ")),"+",time.basis,"+", paste( "(", paste(names(pdat)[ncol(pdat)], collapse=" + ", sep=""), ")", ":", time.basis))  }
+    }
   }
   expSet <- new("ExpressionSet")
   pData(expSet) <- data.frame(pdat)
   exprs(expSet) <- as.matrix(data)
-  edgeObj <- edgeSet(expSet, full.model=as.formula(fmod), null.model=as.formula(nmod), individual=(ind))
+  edgeObj <- edgeSet(expSet, full.model=as.formula(fmod), null.model=as.formula(nmod), individual=ind)
   return(edgeObj)  
 }
